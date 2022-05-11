fd8ac2896182f015fd6c41096e76ad91ac5f2c5e
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-11 20:49:18 +0600

    database connected and collection added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors");
 require('dotenv').config();
 const app = express();
@@ -7,6 +8,25 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.51ygh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("doctorsPortal").collection("services");
+        
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.send("Doctors portal is helping people");
