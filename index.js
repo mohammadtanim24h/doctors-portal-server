e890fc14a8b90af515d8311df53cee5379ac252e
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-15 22:47:44 +0600

    jwt verification function added

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: "UnAuthorized Access"})
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if(err) {
+            return res.status(403).send({message: "Forbidden Access"});
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 
 async function run() {
     try {
@@ -89,13 +104,17 @@ async function run() {
         */
        
         // Get bookings for specific email
-        app.get("/booking", async (req, res) => {
+        app.get("/booking", verifyJWT, async (req, res) => {
             const patientEmail = req.query.email;
-            const authHeader = req.headers.authorization;
-            console.log(authHeader);
-            const query ={patientEmail};
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if(patientEmail === decodedEmail){
+                const query = {patientEmail};
+                const bookings = await bookingCollection.find(query).toArray();
+                return res.send(bookings);
+            }
+            else {
+                return res.status(403).send({message: "Forbidden Access"});
+            }
         })
 
         // Create a booking
